fb43908e19c7663be619f8a4c5427f8c545b9498
CFrankenstein <cfrankenstein@noreply.gitgem.com>
2018-06-10 21:49:42 +0200

    'impressum/index.php' ändern

--- impressum/index.php
+++ impressum/index.php
@@ -9,7 +9,7 @@ require_once("cmsmain.php");
 
 include ('header.php');
 
-$box_title = IMPRESSUM;
+$box_title = _IMPRESSUM;
 $box_firm = $cmsoption[firm];
 $box_holder = $cmsoption[holder];
 $box_street = $cmsoption[street];
@@ -39,23 +39,23 @@ include("themes/$sitetheme/templates/box_content_close.tpl");
 
 if ($language == $cmsoption[languagetwo] AND $language != "" AND $cmsoption[disclaimertwo] != "" AND $cmsoption[copyrighttwo] != "") {
 	if ($cmsoption[disclaimertwo]) {
-		$box_title = IMPRESSUMDISCLAIMER;
+		$box_title = _IMPRESSUMDISCLAIMER;
 		$box_text = html_entity_decode($cmsoption[disclaimertwo], ENT_QUOTES, "UTF-8");
 		include("themes/$sitetheme/templates/box_content.tpl");
 	}
 	if ($cmsoption[copyrighttwo]) {
-		$box_title = IMPRESSUMCOPYRIGHT;
+		$box_title = _IMPRESSUMCOPYRIGHT;
 		$box_text = html_entity_decode($cmsoption[copyrighttwo], ENT_QUOTES, "UTF-8");
 		include("themes/$sitetheme/templates/box_content.tpl");
 	}
 }else{
 	if ($cmsoption[disclaimer]) {
-		$box_title = IMPRESSUMDISCLAIMER;
+		$box_title = _IMPRESSUMDISCLAIMER;
 		$box_text = html_entity_decode($cmsoption[disclaimer], ENT_QUOTES, "UTF-8");
 		include("themes/$sitetheme/templates/box_content.tpl");
 	}
 	if ($cmsoption[copyright]) {
-		$box_title = IMPRESSUMCOPYRIGHT;
+		$box_title = _IMPRESSUMCOPYRIGHT;
 		$box_text = html_entity_decode($cmsoption[copyright], ENT_QUOTES, "UTF-8");
 		include("themes/$sitetheme/templates/box_content.tpl");
 	}
